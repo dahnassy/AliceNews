fcc6a6419aff8e43bf6369cdcf67af190a84ccff
Nathanael de Souza Leite <nathanaelsouzaleite@gmail.com>
2025-06-03 23:40:47 -0300

    atualizações

--- app.config.js
+++ app.config.js
@@ -4,19 +4,37 @@ export default {
     slug: "alice-news",
     version: "1.0.0",
     orientation: "portrait",
-    androidStatusBar: {
-    backgroundColor: "#ffffff",
-    barStyle: "dark-content",
-    },
-    userInterfaceStyle: "automatic",
-    icon: "./src/assets/icon.png",
+    owner: "dahnassy",
+    icon: "./assets/icon.png",
     splash: {
-      image: "./src/assets/splash.png",
-      resizeMode: "contain", // ou "cover"
-      backgroundColor: "#ffffff", // ou escuro, se preferir
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    updates: {
+      fallbackToCacheTimeout: 0
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      package: "com.dahnassy.alicenews", // 👈 adicione isso
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
+    },
+    web: {
+      favicon: "./assets/favicon.png"
     },
     extra: {
-      GNEWS_API_KEY: "fc1da50cb35965e634e4f8bd893e3796"
+      GNEWS_API_KEY: "fc1da50cb35965e634e4f8bd893e3796",
+      eas: {
+        projectId: "368e13f9-24dc-478e-9f07-7f893a15f7b0"
+      }
     }
   }
-}
+};
+
+
